0e5c158519ff4316d9152e42978ebd8a0f76832b
crwilliams87 <chris.r.williams@icloud.com>
2014-06-30 11:16:38 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Zapfoto::Application.config.secret_key_base = '3ea2cf8609851ac532acd25558b208876b5c86d7f818636dd93561f1fd6bd9c93123f3e1315fef5cbf85c26ad9d93120649fd30a632c30cd7bd4c75e6223c8a6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Zapfoto::Application.config.session_store :cookie_store, key: '_zapfoto_session'
