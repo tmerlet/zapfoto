814431c1192f208e711fb36d295c0b672845f966
crwilliams87 <chris.r.williams@icloud.com>
2014-07-01 12:32:10 +0100

    added carrier wave for image uploads

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -71,6 +71,6 @@ class PhotosController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def photo_params
-      params.require(:photo).permit(:image, :roll_id, :latitude, :longitude)
+      params.require(:photo).permit(:image, :latitude, :longitude)
     end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,5 @@
 class Photo < ActiveRecord::Base
   belongs_to :roll
+
+  mount_uploader :image, ImageUploader
 end
