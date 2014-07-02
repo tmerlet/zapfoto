c628a083aa179423d780f503fb379db0a2a9f624
crwilliams87 <chris.r.williams@icloud.com>
2014-07-02 15:30:18 +0100

    added heroku server to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Zapfoto::Application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default :charset => "utf-8"
-  config.action_mailer.default_url_options = { host: 'ADD HEROKU SERVER' }
+  config.action_mailer.default_url_options = { host: 'zapfoto.heroku.com' }
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
     :port      => 25,
