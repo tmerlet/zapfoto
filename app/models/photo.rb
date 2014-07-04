14ea364da8bd8e6b427120af337f758c6e1a6c43
crwilliams87 <chris.r.williams@icloud.com>
2014-07-04 11:40:38 +0100

    created validation for image file type

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,4 +2,6 @@ class Photo < ActiveRecord::Base
   belongs_to :roll
 
   mount_uploader :image, ImageUploader
+
+  validates_format_of :image, :with => %r{\.(png|jpg|jpeg)\A}i, message: "Photo must be .png, .jpg or .jpeg"
 end
